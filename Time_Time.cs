47a274388cbe1b150adc6df8380c85b467667e3b
Yusuke ODA <yus.takara@gmail.com>
2013-02-07 16:11:18 +0900

    Adds Time class.

---
+++ Time_Time.cs
@@ -0,0 +1,16 @@
+﻿namespace MMSystem
+{
+	namespace Time
+	{
+		/// <summary>
+		/// 現在時刻を取得する API 群。
+		/// </summary>
+		public static class Time
+		{
+			public static uint Tick
+			{
+				get { return Win32.Api.timeGetTime(); }
+			}
+		}
+	}
+}
