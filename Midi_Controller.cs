e2e783ca146dc7a2ec0e069a7a553cbaf5d44402
Yusuke ODA <yus.takara@gmail.com>
2013-02-16 09:00:30 +0900

    Fix Controller.Send().

--- Midi_Controller.cs
+++ Midi_Controller.cs
@@ -13,14 +13,13 @@ namespace MMSystem
 			/// <summary>
 			/// 一度に送出できる MIDI メッセージの最大バイト数。
 			/// </summary>
-			public const uint MAX_BYTES_PER_SENDING = 1024;
+			public const int MAX_BYTES_PER_SENDING = 1024;
 
 			/// <summary>
-			/// コントローラの初期化を行う。
-			/// コンストラクタからのみ呼び出される。
+			/// MIDI 出力デバイスのコントローラを生成する。
 			/// </summary>
-			/// <param name="deviceId">MIDI 出力デバイスの ID</param>
-			private void Init(uint deviceId)
+			/// <param name="deviceId">対象となる MIDI 出力デバイスの ID。</param>
+			public Controller(uint deviceId)
 			{
 				Win32.MMRESULT ret;
 
@@ -57,17 +56,12 @@ namespace MMSystem
 			/// <summary>
 			/// MIDI 出力デバイスのコントローラを生成する。
 			/// </summary>
-			/// <param name="deviceId">対象となる MIDI 出力デバイスの ID。</param>
-			public Controller(uint deviceId)
-			{
-				Init(deviceId);
-			}
-
-			/// <summary>
-			/// MIDI 出力デバイスのコントローラを生成する。
-			/// </summary>
-			/// <param name="deviceName">対象となる MIDI 出力デバイスの名前。DeviceCaps.Name の値を使用する。</param>
-			public Controller(string deviceName)
+			/// <param name="deviceName">対象となる MIDI 出力デバイスの名前。DeviceCaps.Name の値を使用できる。</param>
+			/// <returns>
+			/// deviceName で指定した名前のデバイスが見つかった場合は、そのデバイスを表す Controller オブジェクト。
+			/// 見つからなかった場合は null。
+			/// </returns>
+			public static Controller FromName(string deviceName)
 			{
 				// 名前の一致するデバイスを探す
 				uint n = DeviceCaps.NumDevices;
@@ -75,15 +69,12 @@ namespace MMSystem
 				{
 					DeviceCaps caps = new DeviceCaps(i);
 					if (caps.Name == deviceName)
-					{
 						// デバイスを発見したので初期化
-						Init(i);
-						return;
-					}
+						return new Controller(i);
 				}
 
 				// 名前の一致するデバイスがなかった
-				throw new MMSystemException("指定された名前の MIDI 出力デバイスが見つかりませんでした。");
+				return null;
 			}
 
 			/// <summary>
@@ -148,32 +139,34 @@ namespace MMSystem
 			/// デバイスへ指定したバイト数の MIDI メッセージを送出する。
 			/// システム エクスクルーシブ メッセージの送出などに使用する。
 			/// </summary>
-			/// <param name="message">メッセージのバイト列を格納した配列。MAX_BYTES_PER_SENDING 以下の長さである必要がある。</param>
-			public void Send(byte[] message)
+			/// <param name="message">メッセージのバイト列を格納した配列。</param>
+			public void Send(params byte[] message)
 			{
-				if (message.Length > MAX_BYTES_PER_SENDING)
-					throw new ArgumentOutOfRangeException("message", "MIDI メッセージのサイズが MAX_BYTES_PER_SENDING を越えています。");
-
 				int len = message.Length;
 
-				// データのコピー
-				Marshal.Copy(message, 0, buffer_, len);
-
-				unsafe
+				for (int offset = 0; offset < message.Length; offset += MAX_BYTES_PER_SENDING)
 				{
-					// 準備
-					((Win32.MIDIHDR*)hdr_)->dwBufferLength = (uint)len;
-					((Win32.MIDIHDR*)hdr_)->dwFlags = 0;
-					Win32.Api.midiOutPrepareHeader(hmo_, hdr_, cb_hdr_);
+					int sending = Math.Min(message.Length - offset, MAX_BYTES_PER_SENDING);
 
-					// 送出
-					Win32.Api.midiOutLongMsg(hmo_, hdr_, cb_hdr_);
+					// データのコピー
+					Marshal.Copy(message, offset, buffer_, sending);
 
-					// 送出完了まで待機 (ブロッキング)
-					while ((((Win32.MIDIHDR*)hdr_)->dwFlags & Win32.MIDIHDR_FLAGS.MHDR_DONE) == 0) ;
+					unsafe
+					{
+						// 準備
+						((Win32.MIDIHDR*)hdr_)->dwBufferLength = (uint)sending;
+						((Win32.MIDIHDR*)hdr_)->dwFlags = 0;
+						Win32.Api.midiOutPrepareHeader(hmo_, hdr_, cb_hdr_);
 
-					// 後始末
-					Win32.Api.midiOutUnprepareHeader(hmo_, hdr_, cb_hdr_);
+						// 送出
+						Win32.Api.midiOutLongMsg(hmo_, hdr_, cb_hdr_);
+
+						// 送出完了まで待機 (ブロッキング)
+						while ((((Win32.MIDIHDR*)hdr_)->dwFlags & Win32.MIDIHDR_FLAGS.MHDR_DONE) == 0) ;
+
+						// 後始末
+						Win32.Api.midiOutUnprepareHeader(hmo_, hdr_, cb_hdr_);
+					}
 				}
 			}
 
